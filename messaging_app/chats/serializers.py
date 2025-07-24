f7c128aeef7e2e50283e732d1f2990dc04d0377a
JOB-21 <jobabe211@gmail.com>
2025-07-25 00:54:34 +0300

    updated the user in models.py

--- messaging_app/chats/serializers.py
+++ messaging_app/chats/serializers.py
@@ -1,17 +1,26 @@
 from rest_framework import serializers
 from .models import User, Conversation, Message
 
+
 class UserSerializer(serializers.ModelSerializer):
+    phone_number = serializers.CharField(required=True)
+
     class Meta:
         model = User
         fields = ['user_id', 'username', 'email', 'first_name', 'last_name', 'phone_number']
 
+
 class MessageSerializer(serializers.ModelSerializer):
     sender = UserSerializer(read_only=True)
+    message_preview = serializers.SerializerMethodField()
 
     class Meta:
         model = Message
-        fields = ['message_id', 'sender', 'message_body', 'sent_at']
+        fields = ['message_id', 'sender', 'message_body', 'sent_at', 'message_preview']
+
+    def get_message_preview(self, obj):
+        return obj.message_body[:20]  # Return first 20 characters as preview
+
 
 class ConversationSerializer(serializers.ModelSerializer):
     participants = UserSerializer(many=True, read_only=True)
@@ -20,3 +29,8 @@ class ConversationSerializer(serializers.ModelSerializer):
     class Meta:
         model = Conversation
         fields = ['conversation_id', 'participants', 'created_at', 'messages']
+
+    def validate(self, data):
+        if 'participants' not in data or len(data['participants']) < 2:
+            raise serializers.ValidationError("A conversation must include at least two participants.")
+        return data
