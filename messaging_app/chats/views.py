3846b275880667df169cf8ab284742eb439b96ad
JOB-21 <jobabe211@gmail.com>
2025-07-25 01:00:12 +0300

    configured settings.py and added models

--- messaging_app/chats/views.py
+++ messaging_app/chats/views.py
@@ -1,4 +1,4 @@
-from rest_framework import viewsets, status
+from rest_framework import viewsets, status, filters
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
 from .models import Conversation, Message
@@ -10,6 +10,9 @@ class ConversationViewSet(viewsets.ModelViewSet):
     queryset = Conversation.objects.all()
     serializer_class = ConversationSerializer
     permission_classes = [IsAuthenticated]
+    filter_backends = [filters.SearchFilter, filters.OrderingFilter]
+    search_fields = ['participants__username']
+    ordering_fields = ['created_at']
 
     def perform_create(self, serializer):
         serializer.save()
@@ -28,6 +31,9 @@ class MessageViewSet(viewsets.ModelViewSet):
     queryset = Message.objects.all()
     serializer_class = MessageSerializer
     permission_classes = [IsAuthenticated]
+    filter_backends = [filters.SearchFilter, filters.OrderingFilter]
+    search_fields = ['sender__username', 'message_body']
+    ordering_fields = ['sent_at']
 
     def perform_create(self, serializer):
         serializer.save(sender=self.request.user)
